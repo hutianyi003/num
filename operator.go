082a1cb3b38469c7d5255a41df7b498b28e36c57
Tianyi Hu <hutianyi_003@163.com>
2025-07-06 22:20:54 +0800

    feat: add go doc

--- operator.go
+++ operator.go
@@ -1,5 +1,11 @@
+// Package num provides functions to do math.
 package num
 
+// Add computes the sum of two integer values and returns the result.
+//
+// The details can be found at [Addition].
+//
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
