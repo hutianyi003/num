6424bed5c4c18bb0e419f16d61003dbaddf82053
Tianyi Hu <hutianyi_003@163.com>
2025-07-06 22:33:40 +0800

    feat: make Add generic

--- operator.go
+++ operator.go
@@ -1,11 +1,17 @@
 // Package num provides functions to do math.
 package num
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add computes the sum of two integer values and returns the result.
 //
 // The details can be found at [Addition].
 //
 // [Addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
